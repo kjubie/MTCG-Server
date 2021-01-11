cfab8f79c4c905f81aeff167f5f5cb292b95262f
Kjubie Bruckner <abru1322@gmail.com>
2021-01-11 16:33:42 +0100

    CURL Updated

--- Abgabe/MTCG-Server/Effects/Effect.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace MTCG_Server {
-    public abstract class Effect {
-        public string name { get; set; }
-        public string text { get; set; }
-        public abstract void DoBeforeEffect(ref Card opposingCard, ref Card me);
-        public abstract void DoAfterEffect(ref Card opposingCard, int opposingCardCalcDamage, ref Card me, int meCalcDamage, out int damageModifier);
-    }
-}
--- Abgabe/MTCG-Server/Races/Race.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace MTCG_Server {
-    public abstract class Race {
-        public string race;
-        public string text;
-        public abstract void DoRaceEffect(ref Card opposingCard, out int calcDamage);
-    }
-}
